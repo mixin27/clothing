6fce1c1234fb21eba91ed3c6238b687a8623353a
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-02-17 23:03:55 +0630

    split into components and pages, styling menu item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import HomePage from './HomePage';
+import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Directory from '../../components/directory/Directory';
+import './HomePage.styles.scss';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
